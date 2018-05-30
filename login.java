ed6db5ed57383c6f5a5d518ae5fdd6f51360cb52
geek-01 <37298480+geek-01@users.noreply.github.com>
2018-05-30 07:27:24 +0530

    Create login.java

---
+++ login.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pro_ge3k
+ */
+public class login {
+    String uname,pass;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+}
